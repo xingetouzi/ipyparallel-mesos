ef305bf075a7a9c67208328bc8f72db7f446f17e
BurdenBear <541795600@qq.com>
2017-09-03 21:22:09 +0800

    feature: fix bug in start-engine.sh and ipyparallel_mesos

--- docker/engine/start_engine.sh
+++ docker/engine/start_engine.sh
@@ -16,7 +16,6 @@ CONTROLLER_HOST=`curl $MARATHON_MASTER/v2/apps/$CONTROLLER_MARATHON_ID | jq -r '
 echo $CONTROLLER_HOST
 curl http://${CONTROLLER_HOST}:${CONTROLLER_CONFIG_PORT:-1235}/ipcontroller-engine.json --create-dirs -o /opt/profile_mesos/security/ipcontroller-engine.json
 cat /opt/profile_mesos/security/ipcontroller-engine.json
-sudo -E PATH=${PATH} -E IPYTHONPATH=${IPYTHONPATH} -E CONDA_ENVS_PATH=${CONDA_ENVS_PATH} -u ${USER} \
-bash -c "source deactivate && \
-source activate ${CONDA_DEFAULT_ENV} && \
-ipengine --profile=mesos --url=tcp://$HOST:$PORT"
+source deactivate
+source activate ${CONDA_DEFAULT_ENV}
+ipengine --profile=mesos --url=tcp://$HOST:$PORT
