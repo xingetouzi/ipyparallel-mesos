01f63bca44e3f615486181ca5b3723bb132a93c9
BurdenBear <541795600@qq.com>
2017-09-05 00:22:59 +0800

    feature: fix bug in start-engine.sh and ipyparallel_mesos

--- docker/engine/start_engine.sh
+++ docker/engine/start_engine.sh
@@ -1,21 +1,22 @@
 #!/bin/bash 
 
 # 创建用户
-#set -e
-#if getent passwd ${USER_ID} > /dev/null ; then
-#  echo "${USER} (${USER_ID}) exists"
-#else
-#  echo "Creating user ${USER} (${USER_ID})"
-#  useradd -u ${USER_ID} -M -s $SHELL ${USER}
-#fi
-#
-#sudo chown -R ${USER} /opt/conda
+set -e
+if getent passwd ${USER_ID} > /dev/null ; then
+  echo "${USER} (${USER_ID}) exists"
+else
+  echo "Creating user ${USER} (${USER_ID})"
+  useradd -u ${USER_ID} -M -s $SHELL ${USER}
+fi
+
+sudo gpasswd -a ${USER} conda && newgrp conda
 
 # Query marathon find host of ipython paralell controller
 CONTROLLER_HOST=`curl $MARATHON_MASTER/v2/apps/$CONTROLLER_MARATHON_ID | jq -r '.app.tasks | .[0].host'`
 echo $CONTROLLER_HOST
 curl http://${CONTROLLER_HOST}:${CONTROLLER_CONFIG_PORT:-1235}/ipcontroller-engine.json --create-dirs -o /opt/profile_mesos/security/ipcontroller-engine.json
 cat /opt/profile_mesos/security/ipcontroller-engine.json
-source deactivate
-source activate ${CONDA_DEFAULT_ENV}
-ipengine --profile=mesos --url=tcp://$HOST:$PORT
+sudo -E PATH=${PATH} -E IPYTHONPATH=${IPYTHONPATH} -E CONDA_ENVS_PATH=${CONDA_ENVS_PATH} -u ${USER} \
+bash -c "source deactivate && \
+source activate ${CONDA_DEFAULT_ENV} && \
+ipengine --profile=mesos --url=tcp://$HOST:$PORT"
